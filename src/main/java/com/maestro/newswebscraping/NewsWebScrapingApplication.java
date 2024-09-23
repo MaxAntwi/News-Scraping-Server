08d2e5da86840ef781fc97cc0ff1f32dbcc564aa
Maxwell Antwi <theonlymaxmc@gmail.com>
2024-09-23 09:08:31 -0700

    initial commit

---
+++ src/main/java/com/maestro/newswebscraping/NewsWebScrapingApplication.java
@@ -0,0 +1,13 @@
+package com.maestro.newswebscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebScrapingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsWebScrapingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maestro/newswebscraping/NewsWebScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maestro.newswebscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebScrapingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
